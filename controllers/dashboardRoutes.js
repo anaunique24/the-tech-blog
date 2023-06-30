0ee15f3265ad808165569adccdb540398ca19996
Ana Grisham <anaunique@icloud.com>
2023-06-29 17:20:02 -0700

    created dashboard route

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,11 +1,74 @@
 const router = require('express').Router();
-const {Posts, User, Comment} = require('../models');
+const { Posts, User, Comment } = require('../models');
 const auth = require('../utils/auth');
 
-// router.get('/', auth, async (req, res) => {
-//     try{
-//         const dashboardData = await 
-//     } catch (error) {
-//         res.status(500).json(error);
-//     };
-// });
+router.get('/', auth, async (req, res) => {
+    try{
+        const dashboardData = await Posts.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        const newDashboard = dashboardData.map(dashboard => dashboard.get({ plain: true}));
+    res.render('dashboard', {newDashboard})
+    } catch (error) {
+        res.status(500).json(error);
+    };
+});
+
+router.get('/edit/:id', auth, async (req, res) => {
+    try{
+        const postData = await Posts.findByPk({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        })
+        const newPostData = postData.map(post => post.get ({ plain:true }));
+        res.render('edit', {newPostData});
+    } catch (error) {
+        res.status(500).json(error);
+    };
+});
+
+router.get('/new', (req, res) => {
+    res.render('new-post');
+});
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,23 +4,110 @@ const auth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try{
-    const postData = await Posts.findAll();
+    const postData = await Posts.findAll({
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [{
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
     const newPostData = postData.map(post => post.get({ plain: true}));
-    res.render('allposts', {newPostData})
+    res.render('homepage', {newPostData})
     } catch (error) {
         res.status(500).json(error)
     };
 });
 
-router.get('/profile', auth ,async (req, res) => {
-    const userData = await User.findByPk(req.session.user_id, {
-        include: [{
-            model: Posts,
-            attributes: ['title', 'content']
-        }]
-    });
-    let newUserData = userData.get({plain: true});
-    res.render('profile', newUserData);
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/signup', async (req, res) => {
+    res.render('signup');
+});
+
+router.get('post/:id', async (req, res) => {
+    try{
+        const postData = await Posts.findByPk({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'content',
+                'title',
+                'created_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        const newPostData = postData.map(post => post.get({ plain: true}));
+        res.render('single-post', {newPostData})
+    } catch (error) {
+        res.status(500).json(error)
+    };
+});
+
+router.get('/comments', async (req, res) => {
+    try{
+        const commentData = await Comment.findByPk({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'content',
+                'title',
+                'created_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        const newCommentData = commentData.map(comment => comment.get({plain:true}));
+        res.render('comment', {newCommentData});
+    } catch (error) {
+        res.status(500).json(error)
+    };
 });
 
 
