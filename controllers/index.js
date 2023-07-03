d089642a857be7e34b1d23995b30eea1be937ce9
Ana Grisham <anaunique@icloud.com>
2023-07-03 13:41:48 -0700

    started post front end

--- controllers/index.js
+++ controllers/index.js
@@ -8,9 +8,5 @@ router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
 
-router.use((req, res) => {
-    res.status(404).end();
-});
-
 
 module.exports = router;
